04ee528bc3b4f5dc30f98c48a117f3364b7ff900
Laurin Boujon <boujon.laurin@gmail.com>
2026-01-26 16:12:15 +0100

    refactor: fix rss backend and clean legacy

--- packages/api/app/utils/__init__.py
+++ packages/api/app/utils/__init__.py
@@ -1,11 +1,9 @@
 """Utilitaires."""
 
-from app.utils.rss_parser import RSSParser
 from app.utils.youtube_utils import extract_youtube_channel_id, get_youtube_rss_url
 from app.utils.duration_estimator import estimate_reading_time
 
 __all__ = [
-    "RSSParser",
     "extract_youtube_channel_id",
     "get_youtube_rss_url",
     "estimate_reading_time",
