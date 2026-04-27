22954adc0d646d569b774b38b4648f58e4d6978c
boujonlaurin-dotcom <boujonlaurin-dotcom@users.noreply.github.com>
2026-04-27 22:25:54 +0000

    deploy: 3b6885774ba3f979742163bc14891cbba0e0d687

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/assets/loaders/loading_facteur.json": "2ad70ee4008c82
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0962f4c72517266aa30cac606590bcb4",
+"flutter_bootstrap.js": "15c7fd05b86bc2fbe00c97525e5fb852",
 "main.dart.js": "33090f5690ab8a96b7d646fd0468bba7",
 "version.json": "23790d96eb676a5e9516ef2c8bac14dd",
 "icons/Icon-maskable-512.png": "cae642f390c6970d03aaa5134ddbee18",
