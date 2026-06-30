8c705fd771eae0d584876b7c464d37d0903b4476
boujonlaurin-dotcom <boujonlaurin-dotcom@users.noreply.github.com>
2026-06-30 11:02:33 +0000

    deploy: b9d87546b9a8205dd173e204623569a54d220880

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "591828280"
+    serviceWorkerVersion: "2462778661"
   }
 });
