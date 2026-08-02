7112fdb4ff44b95a834e15cc206df208a8761f55
boujonlaurin-dotcom <boujonlaurin-dotcom@users.noreply.github.com>
2026-08-02 20:54:07 +0000

    deploy: 259ee5ec286148d01c36b9f36e01dc71ca0c490d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "159fa956f52abe713fdffa560cf340be",
-"flutter_bootstrap.js": "8e8ccb53f7b2e899f89ea4577eb091fb",
+"flutter_bootstrap.js": "3211caf8bbb4889418e1a543abded996",
 "icons/Icon-192.png": "4707a087c6c2650cebb4297382e64d97",
 "icons/Icon-512.png": "d94321ef49ccb865e1c8efd333a31049",
 "icons/Icon-maskable-512.png": "d94321ef49ccb865e1c8efd333a31049",
