2e704462af6e667a0b4bad21a746afc939921ee8
djbouchez-pixel <djbouchez-pixel@users.noreply.github.com>
2026-08-09 18:29:20 +0000

    preview(web): claude/facteur-article-swipe-nav-cd6pv0 → /preview/swipe-nav/ e0ec4bf5584d39351699a05798df0a37d5400465

--- preview/swipe-nav/flutter_service_worker.js
+++ preview/swipe-nav/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "159fa956f52abe713fdffa560cf340be",
-"flutter_bootstrap.js": "e8e6332c470537a0e378251964806737",
+"flutter_bootstrap.js": "955dbaab7012d440fd54328e4d123092",
 "icons/Icon-192.png": "4707a087c6c2650cebb4297382e64d97",
 "icons/Icon-512.png": "d94321ef49ccb865e1c8efd333a31049",
 "icons/Icon-maskable-512.png": "d94321ef49ccb865e1c8efd333a31049",
