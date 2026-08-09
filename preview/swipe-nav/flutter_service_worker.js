d827a191719e6b7faaf16c7160b545507742968c
djbouchez-pixel <djbouchez-pixel@users.noreply.github.com>
2026-08-09 15:18:10 +0000

    preview(web): claude/facteur-article-swipe-nav-cd6pv0 → /preview/swipe-nav/ 4a49cd4916bb22b7b3490e727472fbe1df627a5f

--- preview/swipe-nav/flutter_service_worker.js
+++ preview/swipe-nav/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "159fa956f52abe713fdffa560cf340be",
-"flutter_bootstrap.js": "1a39d768a4067ba66ddba69775809caa",
+"flutter_bootstrap.js": "f5f89130e0ecf1d214441e390e99edca",
 "icons/Icon-192.png": "4707a087c6c2650cebb4297382e64d97",
 "icons/Icon-512.png": "d94321ef49ccb865e1c8efd333a31049",
 "icons/Icon-maskable-512.png": "d94321ef49ccb865e1c8efd333a31049",
@@ -12,7 +12,7 @@ const RESOURCES = {"favicon.png": "159fa956f52abe713fdffa560cf340be",
 "index.html": "4cbce511cd7a30488e60e5b09c18dea2",
 "/": "4cbce511cd7a30488e60e5b09c18dea2",
 "email-confirmation.html": "ac115775cbd8a949a4c666e687510e43",
-"main.dart.js": "7b0e566e50a6a9578e47b8a5a9103857",
+"main.dart.js": "bd1b57901dd02822887f49e67441e3e4",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.wasm": "7e5f3afdd3b0747a1fd4517cea239898",
 "canvaskit/skwasm.js": "8060d46e9a4901ca9991edd3a26be4f0",
