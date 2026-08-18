a1759f54d44c120fc1edfb2b28a0899bd7b88c38
boujonlaurin-dotcom <boujonlaurin-dotcom@users.noreply.github.com>
2026-08-18 21:03:25 +0000

    deploy: a6b53ed9f33ee675f4857865904a57aeb59b103b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -67,7 +67,7 @@ const RESOURCES = {"version.json": "f054a6f707f540f8ea20652a32028be8",
 "reset-password.html": "55abf02dbadfe0e9c531305c62fbf29d",
 "main.dart.js": "5095714f2648e4fab84a110173bed9e5",
 "email-confirmation.html": "ac115775cbd8a949a4c666e687510e43",
-"flutter_bootstrap.js": "49e78f9be04a817756d536f618d017c9",
+"flutter_bootstrap.js": "e3b7ddf1da7dc06797f39529b00df7b0",
 "manifest.json": "d7eddc4cd63df39a919eddaf81649c6f"};
 // The application shell files that are downloaded before a service worker can
 // start.
