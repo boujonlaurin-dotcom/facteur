9b2da810b0b80e4d19580c3594fe1b655b5706a1
boujonlaurin-dotcom <boujonlaurin-dotcom@users.noreply.github.com>
2026-08-19 19:57:36 +0000

    deploy: a7f1612c60281641c733537c35ea8db0eddfb69d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3465362245"
+    serviceWorkerVersion: "4180054690"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,9 +65,9 @@ const RESOURCES = {"version.json": "f054a6f707f540f8ea20652a32028be8",
 "assets/packages/phosphor_flutter/lib/fonts/Phosphor-Bold.ttf": "8fedcf7067a22a2a320214168689b05c",
 "assets/packages/phosphor_flutter/lib/fonts/Phosphor.ttf": "003d691b53ee8fab57d5db497ddc54db",
 "reset-password.html": "55abf02dbadfe0e9c531305c62fbf29d",
-"main.dart.js": "118f44a47c188b21ce8de282c6f1f57d",
+"main.dart.js": "c5048d434c89d890b3f769a64a31d7d6",
 "email-confirmation.html": "ac115775cbd8a949a4c666e687510e43",
-"flutter_bootstrap.js": "35190bb534e5a18e0946dfae73901e4c",
+"flutter_bootstrap.js": "20cede1d3d0c0eec322cb0461c7728b0",
 "manifest.json": "d7eddc4cd63df39a919eddaf81649c6f"};
 // The application shell files that are downloaded before a service worker can
 // start.
